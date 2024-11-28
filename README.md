230c4523d7117ff55734a178d4e25320bd8fce69
wonss737 <wonhyeok737@gmail.com>
2024-11-28 15:25:31 +0900

    FM24 matchengine maker v3.0.0
    :Detailed Notes:
      - main.py
        - v1.0.0
          - main.py read json file then update physical_constraints.jsb
        - v2.0.0
          - Value about "shirt_tug" has error, so remove it
          - Update physical_constraints.json file for user freindly
        - v3.0.0
          - Add filtering non-exist values
          - Clip "shirt_tug_do" value from 0 to 255
    
    :Testing Steps:
      - pyinstall main.py into main.exe
      - Run FM24

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # fm24-matchengine-maker
-FM24 matchengine maker &amp; edit other fmf files
+
+여기를 수정하면 포지션 늘어도 어빌이 안 줄지 않을까
+    comp editor/format/database/player.xml
+    comp editor/format/database ui/person ui.xml
+        Current Ability-Dependent Attributes
---
+++ tool/check_trait.py
@@ -0,0 +1,54 @@
+import os
+import logging
+
+from main import load
+
+def find_key(data_all):
+    for i in range(len(data_all)):
+        data = data_all[i]
+        try:
+            char = data.decode("utf-8")
+            if char < "A" or char > "z":
+                continue
+            else:
+                yield(char)
+        except:
+            continue
+
+
+path = "db/fmf_original"
+output_dir = "traits"
+
+exclude = [
+    "backup",
+    "23_1_1_9",
+    "23_7_0_5",
+    "23_9_9_1",
+]
+
+for dir_name, dir_list, file_list in os.walk(path, followlinks=True):
+    dont_care = False
+    for exclude_dir in exclude:
+        if exclude_dir in dir_name:
+            dont_care = True
+    if dont_care:
+        continue
+
+    for file_name in file_list:
+        if not file_name.endswith(".jsb"):
+            continue
+        file_path = os.path.join(dir_name, file_name)
+        data_all = load(file_path)
+
+        output_txt = "".join(file_name.split(".")[:-1])
+        output_txt = dir_name.replace("/", "_") + "_" + output_txt
+        output_txt = os.path.join(output_dir, output_txt + ".txt")
+
+        with open(output_txt, "w") as wf:
+            count = 0
+            keys = find_key(data_all)
+            for key in keys:
+                if count % 1000 == 0:
+                    wf.write("\n")
+                wf.write(key)
+                count += 1
